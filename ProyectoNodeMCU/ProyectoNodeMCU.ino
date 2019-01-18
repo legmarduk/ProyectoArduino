1bc9bfdef3ff5b3b93869672ac80564d3a94871a
ian lopez candia <ian.lopez.c@gmail.com>
2019-01-18 13:49:08 -0300

    Codigo Final Proyecto Arduino

---
+++ ProyectoNodeMCU/ProyectoNodeMCU.ino
@@ -0,0 +1,89 @@
+#include "DHT.h"
+#include <ESP8266WiFi.h>
+
+/*Pines definidos para sensores*/
+#define PINDHT D7
+#define PINHT A0
+
+/*datos WiFi*/
+const char* ssid = "Nelson Daniel";
+const char* password ="03Mayo1997";
+const char* host = "192.168.43.126"; /*mi ip*/ 
+
+DHT dht(PINDHT,DHT11);
+
+void setup() {
+ Serial.begin(115200);
+ dht.begin();
+ WiFi.begin(ssid,password);
+
+ while(WiFi.status() != WL_CONNECTED)
+ {
+    Serial.print(".");
+    delay(500);
+ }
+ Serial.println();
+ Serial.print("WIFI IP: ");
+ Serial.println(WiFi.localIP());
+ Serial.println("/////////////");
+ Serial.println();
+}
+
+void loop() 
+{
+  WiFiClient client;
+  const int port = 80;
+
+  if(!client.connect(host, port))
+  {
+        Serial.println("Fallo");
+        return;  
+  }
+  else
+  {
+       Serial.println("Conectado");     
+       float temperatura = getTemperatura();
+       float humedad = getHumedad();
+       float humedadTierra= getHumedadTierra();
+      
+       String url =  "/Arduino/Datos.php";
+       String dato1 = "?t=";
+       String dato2 = "&h=";
+       String dato3 = "&ht=";
+       Serial.print("Url: ");
+       Serial.println(url);
+    
+       client.print(String("GET ") + url  +dato1 + temperatura + dato2 +humedad+dato3+humedadTierra+ " HTTP/1.1\r\n"+ "Host: " + host + "\r\n" +"Connection: close\r\n\r\n");
+             
+       unsigned long timeout = millis();
+      
+       while(client.available() == 0)
+       {
+          if(millis() - timeout >5000)
+          {
+            Serial.print("Tiempo muerto");
+            client.stop();
+            return;
+          }  
+        }
+    
+       while(client.available())
+       {
+        String line = client.readStringUntil('\r');
+        Serial.print(line); 
+       }  
+    
+     Serial.println();
+     Serial.println("Conexion cerrada");
+  }
+  delay(20000);                     
+}
+
+/*Recopilar Datos de Los Sensores*/
+float getTemperatura(){ return dht.readTemperature();}
+float getHumedad(){return dht.readHumidity();}
+float getHumedadTierra()
+{
+  float  ht= analogRead(PINHT);
+  return((map(ht, 0, 1023, 100, 0)));
+}
---
+++ ProyectoNodeMCU/db.sql
@@ -0,0 +1,7 @@
+CREATE TABLE datos(
+    id_datos INT  AUTO_INCREMENT PRIMARY KEY,
+    temperatura FLOAT,
+    humedad FLOAT,
+    humedad_tierra FLOAT,
+    fecha DATETIME
+);
